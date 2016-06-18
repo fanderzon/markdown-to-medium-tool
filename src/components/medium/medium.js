b7f9e05d21d9859d2ee9612cc342edec3962ca15
Fredrik Andersson <fredrik.andersson@widespace.com>
2016-06-18 20:57:46 +0200

    Markdown parsing done with marked library

--- src/components/medium/medium.js
+++ src/components/medium/medium.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './medium.css';
 import { connect } from 'react-redux';
+import converter from '../../converter.js';
 
 const Medium = props => {
+  let content = {
+    __html: converter(props.content)
+  }
   return (
-    <div className={styles.container}>
-      {props.content}
-    </div>
+    <div className={styles.container} dangerouslySetInnerHTML={content}></div>
   );
 };
 
