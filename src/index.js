d80b88b7c978bd1d2a7b1bd83a46705cd277e26c
Fredrik Andersson <fredrik.andersson@widespace.com>
2016-06-18 13:00:09 +0200

    Basic webpack react babel setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+export const App = (props) => {
+  return <div>Hello worlds</div>
+};
+
+ReactDOM.render(<App />, document.querySelector('#app'));
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+var path = require('path');
+var loaders = require('./webpack.loaders');
+
+module.exports = {
+	entry: [
+		'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
+		'webpack/hot/only-dev-server',
+		'./src/index.js' // Your appʼs entry point
+	],
+	devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'app.js'
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
+	module: {
+		loaders: loaders
+	},
+	devServer: {
+		contentBase: "./dist",
+			noInfo: false, //  --no-info option
+			hot: true,
+			inline: true
+		},
+	plugins: [
+		new webpack.NoErrorsPlugin()
+	]
+};
